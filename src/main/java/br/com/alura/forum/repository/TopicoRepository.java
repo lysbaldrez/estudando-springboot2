8c5293a96d28b655f81e7aac902189296ad67fd5
lysbaldrez <lyscalefi@gmail.com>
2020-09-08 10:04:51 -0400

    Consultando com filtros

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,9 +1,18 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
+
+    List<Topico> findByCursoNome(String nomeCurso);
+    
+//    Outro jeito de escrever o código acima sem usar o find
+//    @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+//    List <Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+    
 
 }
