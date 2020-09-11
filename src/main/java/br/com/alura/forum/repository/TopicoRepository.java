b8d3d7f1247e3dace5d7b5f1d9f48056d3a083e2
lysbaldrez <lyscalefi@gmail.com>
2020-09-11 12:29:38 -0400

    Paginação e ordenação

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -2,13 +2,15 @@ package br.com.alura.forum.repository;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
 
-    List<Topico> findByCursoNome(String nomeCurso);
+    Page <Topico> findByCursoNome(String nomeCurso, org.springframework.data.domain.Pageable paginacao);
     
 //    Outro jeito de escrever o código acima sem usar o find
 //    @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
