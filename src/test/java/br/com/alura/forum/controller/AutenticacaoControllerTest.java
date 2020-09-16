c4b3f1dbc8fbff1d6e4ad1b0d362703fbdbf741d
lysbaldrez <lyscalefi@gmail.com>
2020-09-16 10:52:00 -0400

    Arrumando testes

--- src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
+++ src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
@@ -1,25 +1,19 @@
 package br.com.alura.forum.controller;
 
-import static org.junit.Assert.*;
-
 import java.net.URI;
-import java.net.URISyntaxException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.google.common.net.MediaType;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,7 +25,7 @@ public class AutenticacaoControllerTest {
     private MockMvc mockMvc;
     
     @Test
-    public void deveriaDevolver400CasoDadosDeAutenticacaoEstejamIncorretos() throws URISyntaxException{
+    public void deveriaDevolver400CasoDadosDeAutenticacaoEstejamIncorretos() throws Exception{
         URI uri = new URI ("/auth");
         String json = "{\"email\":\"invalido@email.com\",\"senha\":123456}\"";
         
